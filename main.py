c99cbe29e4f7fc14c27cfadcb668f03c722889ad
rachit <rachit.anand04111@outlook.com>
2024-09-25 10:32:58 +0530

    added chess pieces

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 import pygame
+import sys
 from chessboard import ChessBoard
 
 pygame.init()
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+width, height = 600, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Chess Board')
+
+chess_board = ChessBoard(screen, width, height)
+
 clock = pygame.time.Clock()
 running = True
-
-chess_board = ChessBoard(screen, SCREEN_WIDTH, SCREEN_HEIGHT)
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+
     screen.fill((255, 255, 255))
-    
     chess_board.construct_board()
+    chess_board.draw_pieces()
     
-            
     pygame.display.flip()
     clock.tick(60)
-    
+
 pygame.quit()
+sys.exit()
